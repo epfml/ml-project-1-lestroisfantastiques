80ad92cb38f1b7c74e5336ad0de89137ccba5c7e
Proche78 <pauline.roche@epfl.ch>
2023-10-19 15:17:49 +0200

    yo

---
+++ dataset.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append(  "../../ml-project-1-lestroisfantastiques")  # This goes up twice in the directories tree (hence in labs)
+# then goes down to ex02/template where your files from lab 2 are.
+
+
+
+
