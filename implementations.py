bf865a0a8be77948ee01bd76880dd109233901b9
Proche78 <92581804+Proche78@users.noreply.github.com>
2023-10-19 15:58:13 +0200

    Add mean_squared_error
    functions :
    calculate_mse(e)
    calculate_mae(e)
    
    
    A CHECK : vérifier qu'il s'agit du dernier w

--- implementations.py
+++ implementations.py
@@ -1,1 +1,95 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
 
+%load_ext autoreload
+%autoreload 2
+
+
+  def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
+
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    
+    #compute loss by MSE
+    e = y - tx.dot(w)
+    loss =  calculate_mse(e)  
+    return loss
+        
+   
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    #compute gradient vector    
+    N = len(y)    
+    e = y - tx.dot(w) 
+    gradient = -1/N * tx.T.dot(e)
+    
+    return gradient
+
+   
+
+
+
+def mean_squarred_error_gd(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    gradient = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute gradient and loss        
+        gradient = compute_gradient(y,tx,w)      
+        loss = compute_loss(y, tx, w)  #loss = 1/(2*len(y)) * np.dot(e.T, e)
+
+        #  update w by gradient
+        w = w -gamma * gradient
+        
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+       
+      print(
+            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
